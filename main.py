2dc76fca4e8b688da642d83cb8c01623729d99f9
mozheikos <mozheiko.stanislav@yandex.ru>
2022-02-27 14:19:21 +0300

    fill_all_tables

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from pycoingecko import CoinGeckoAPI
 import json
+from pycoingecko import CoinGeckoAPI
+from math import ceil
+from time import sleep
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
 from tables_creator import Coins, Exchanges, TradeItems, engine, create_tables, Base
 
 cg = CoinGeckoAPI()
@@ -12,45 +13,71 @@ create_tables(Base, engine)
 session = Session(bind=engine)
 
 def fill_table(iter_item, function):
+    ids = []
     for item in iter_item:
-        row = function(item)
-        session.add(row)
+        row, item_id = function(item)
+        if item_id:
+            ids.append(item_id)
+        if row:
+            session.add(row)
     session.commit()
+    return ids
 
 def get_coin_first_run(cg_coin):
+    rank = cg_coin['market_cap_rank']
+    links = json.dumps(cg.get_coin_by_id(cg_coin['id'])['links'])
+    if not rank:
+        return None
     coin = Coins(
         name = cg_coin['name'],
         ticker = cg_coin['symbol'],
         coingecko_id = cg_coin['id'],
-        coin_rank = cg_coin['market_cap_rank'],
+        coin_rank = rank,
         price = cg_coin['current_price'],
+        total_volume = cg_coin['total_volume'],
         circulating_supply = cg_coin['circulating_supply'],
         total_supply = cg_coin['total_supply'],
+        links = links,
     )
-    return coin
+    return coin, coin.coingecko_id
 
-def get_exchanges(exch):
+def get_exchange(exch):
+    trust = exch['trust_score']
+    if not trust:
+        return None
     exchange = Exchanges(
         coingecko_id = exch['id'],
         display_symbol = exch['id'],
         exchange_name = exch['name'],
-        trust_score = exch['trust_score'],
     )
-    return exchange
+    return exchange, exchange.coingecko_id
 
-# def get_trade_items(item):
-#     base_id = session.query(Coins).filter(Coins.coingecko_id == item['coin_id']).one().id
-#     target_id = session.query(Coins).filter(Coins.coingecko_id == item['target_coin_id']).one().id
-#     exchange_id = session.query(Exchanges).filter(Exchanges.coingecko_id == )
+def get_trade_items(item):
+    coin = session.query(Coins).filter(Coins.coingecko_id == item['coin_id']).one_or_none()
+    exchange = session.query(Exchanges).filter(Exchanges.coingecko_id == item['market']['identifier']).one_or_none()
+    volume_percentage = round(item['converted_volume']['usd'] / float(coin.total_volume) * 100, 2)
+    if not coin:
+        return None, None
+    trust = item['trust_score']
+    if not trust:
+        return None, None
+    tradeitem = TradeItems(
+        coin_id = coin.id,
+        exchange_id = exchange.id,
+        coin_symbol = f"{item['base']}/{item['target']}",
+        volume = item['converted_volume']['usd'],
+        volume_percentage = volume_percentage,
+        trust_score = trust,
+    )
+    return tradeitem, None
 
-#     oin_id = base_id,
-#     target_coin_id = target_id,
-#     exchange_id = Column(Integer, ForeignKey('exchanges.id'))
-#     coin_symbol = Column(String(40), default=('coin_id.ticker' + '/' + 'target_coin_id.ticker'))
+coins = cg.get_coins_markets('usd', per_page=40)
+coins_ids = fill_table(coins, get_coin_first_run)
 
+exchanges = cg.get_exchanges_list()[:40]
+exchanges_ids = fill_table(exchanges, get_exchange)
 
-coins = cg.get_coins_markets('usd')
-fill_table(coins, get_coin_first_run)
-
-exchanges = cg.get_exchanges_list()
-fill_table(exchanges, get_exchanges)
+tickers_ids = []
+for item in exchanges_ids:
+    tickers_list = cg.get_exchanges_tickers_by_id(item, coin_ids=coins_ids)['tickers']
+    tickers_ids += fill_table(tickers_list, get_trade_items)
--- tables_creator.py
+++ tables_creator.py
@@ -1,6 +1,6 @@
-from sqlalchemy.orm import relationship, session
-from sqlalchemy import BigInteger, Index, UniqueConstraint, create_engine,\
-     MetaData, Table, Integer, String, Column, DateTime, ForeignKey, Numeric, JSON, Boolean
+from sqlalchemy.dialects import postgresql
+from sqlalchemy import JSON, BigInteger, Index, create_engine,\
+     MetaData, Integer, String, Column, ForeignKey, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine("postgresql://parser:parser@localhost:5432/analyticsgroupdb")
@@ -15,16 +15,16 @@ class Coins(Base):
     __tablename__ = 'coins'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
-    ticker = Column(String(10), nullable=False)
+    ticker = Column(String, nullable=False)
     coingecko_id = Column(String(100), nullable=False)
     coin_rank = Column(Integer)
     price = Column(Numeric)
-    circulating_supply = Column(BigInteger)
-    total_supply = Column(BigInteger)
+    total_volume = Column(postgresql.NUMERIC)
+    circulating_supply = Column(postgresql.NUMERIC)
+    total_supply = Column(postgresql.NUMERIC)
+    links = Column(JSON)
 
     __table_args__ = (
-        UniqueConstraint('name'),
-        UniqueConstraint('ticker'),
         Index('ticker_idx', 'ticker'),
     )
 
@@ -32,18 +32,19 @@ class Coins(Base):
 class Exchanges(Base):
     __tablename__ = 'exchanges'
     id = Column(Integer, primary_key=True)
-    coingecko_id = Column(String(30))
-    display_symbol = Column(String(30))
-    exchange_name = Column(String(255))
-    trust_score = Column(Integer)
+    coingecko_id = Column(String(255), nullable=False)
+    display_symbol = Column(String(255), nullable=False)
+    exchange_name = Column(String(255), nullable=False)
 
 class TradeItems(Base):
     __tablename__ = 'coins_exchanges'
     id = Column(Integer, primary_key=True)
     coin_id = Column(Integer, ForeignKey('coins.id'))
-    target_coin_id = Column(Integer, ForeignKey('coins.id'))
     exchange_id = Column(Integer, ForeignKey('exchanges.id'))
-    coin_symbol = Column(String(40), default=('coin_id.ticker' + '/' + 'target_coin_id.ticker'))
+    coin_symbol = Column(String(255))
+    volume = Column(BigInteger)
+    volume_percentage = Column(postgresql.NUMERIC)
+    trust_score = Column(String(15))
 
 def create_tables(base_class, engine):
     base_class.metadata.create_all(engine, checkfirst=True)
